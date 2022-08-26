3d50c17a4a4fb0e64518570f14029332b82a769b
s-cryptor <grisga2001@gmail.com>
2022-08-26 19:50:39 +0300

    make base structure, add font

--- app.vue
+++ app.vue
@@ -1,5 +1,21 @@
 <template>
   <div>
-    <NuxtWelcome />
+    <Head>
+      <Title>Stone store</Title>
+    </Head>
+    <NuxtPage />
   </div>
 </template>
+
+<style>
+@import url('https://rsms.me/inter/inter.css');
+
+html {
+  font-family: 'Inter', sans-serif;
+}
+@supports (font-variation-settings: normal) {
+  html {
+    font-family: 'Inter var', sans-serif;
+  }
+}
+</style>
